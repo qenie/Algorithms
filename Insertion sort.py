4d8cb656f4fb8dc22944eb970e3b3a4f067353ec
qenie <47893217+qenie@users.noreply.github.com>
2019-03-06 20:44:10 -0500

    Update Insertion sort.py

--- Insertion sort.py
+++ Insertion sort.py
@@ -1,1 +1,20 @@
+# Insertion sort
 
+data = [2, 5, 7, 1]
+
+
+def insertion_sort(array):
+    for index in range(1, len(array)):
+        current = array[index]
+        position = index
+
+        while position > 0 and array[position-1] > current:
+            array[position] = array[position-1]
+            position -= 1
+
+        array[position] = current
+
+    return array
+
+
+print(insertion_sort(data))
